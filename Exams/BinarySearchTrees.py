fd41478d975152b4143bd9e36f5e3544dd3039a6
few717254 <few717254@gmail.com>
2026-07-01 08:22:26 +0700

    refactor: clean up BinarySearchTree implementation by removing redundant code and improving structure

--- Exams/BinarySearchTrees.py
+++ Exams/BinarySearchTrees.py
@@ -140,13 +140,64 @@
 #     preOrder(tree.root)
 
 
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+    
+
+# class BinarySearchTree:
+#     def __init__(self):
+#         self.root = None
+    
+#     def insert(self, value):
+#         if self.root is None: self.root = Node(value)
+#         else: self._insert_recursive(self.root, value)
+    
+#     def _insert_recursive(self, current_node: Node, value):
+#         if value < current_node.value:
+#             if current_node.left is None: current_node.left = Node(value)
+#             else: self._insert_recursive(current_node.left, value)
+#         elif value > current_node.value:
+#             if current_node.right is None: current_node.right = Node(value)
+#             else: self._insert_recursive(current_node.right, value)
+#         else: pass
+    
+#     def delete(self, value):
+#         self.root = self._delete_recursive(self.root, value)
+    
+#     def _delete_recursive(self, current_node: Node, value):
+#         if current_node is None: return current_node
+#         if value < current_node.value: current_node.left = self._delete_recursive(current_node.left, value)
+#         elif value > current_node.value: current_node.right = self._delete_recursive(current_node.right, value)
+#         else:
+#             if current_node.left is None: return current_node.right
+#             elif current_node.right is None: return current_node.left
+
+#             temp = self._min_value_node(current_node.right)
+#             current_node.value = temp.value
+#             current_node.right = self._delete_recursive(current_node.right, temp.value)
+
+#         return current_node
+
+#     def _min_value_node(self, current_node: Node):
+#         if current_node.left is None : return current_node
+#         return self._min_value_node(current_node.left)
+
+# def preOrder(root: Node):
+#     if root is not None:
+#         print(root.value, end = "")
+#         preOrder(root.left)
+#         preOrder(root.right)
+
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
     
-
 class BinarySearchTree:
     def __init__(self):
         self.root = None
@@ -156,40 +207,9 @@ class BinarySearchTree:
         else: self._insert_recursive(self.root, value)
     
     def _insert_recursive(self, current_node: Node, value):
-        if value < current_node.value:
-            if current_node.left is None: current_node.left = Node(value)
-            else: self._insert_recursive(current_node.left, value)
-        elif value > current_node.value:
-            if current_node.right is None: current_node.right = Node(value)
-            else: self._insert_recursive(current_node.right, value)
-        else: pass
-    
-    def delete(self, value):
-        self.root = self._delete_recursive(self.root, value)
-    
-    def _delete_recursive(self, current_node: Node, value):
-        if current_node is None: return current_node
-        if value < current_node.value: current_node.left = self._delete_recursive(current_node.left, value)
-        elif value > current_node.value: current_node.right = self._delete_recursive(current_node.right, value)
-        else:
-            if current_node.left is None: return current_node.right
-            elif current_node.right is None: return current_node.left
+        pass
 
-            temp = self._min_value_node(current_node.right)
-            current_node.value = temp.value
-            current_node.right = self._delete_recursive(current_node.right, temp.value)
 
-        return current_node
-
-    def _min_value_node(self, current_node: Node):
-        if current_node.left is None : return current_node
-        return self._min_value_node(current_node.left)
-
-def preOrder(root: Node):
-    if root is not None:
-        print(root.value, end = "")
-        preOrder(root.left)
-        preOrder(root.right)
 
 if __name__ == "__main__":
     # tree = BinarySearchTree()
